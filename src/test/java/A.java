24b6d147a600905cc0394470b83d0883f587ebc0
meenu189 <sathyabhavan20@gmail.com>
2023-10-25 20:22:30 +0530

    added a class A

---
+++ src/test/java/A.java
@@ -0,0 +1,15 @@
+
+public class A {
+
+	private void methodA1() {
+		System.out.println("methodA1");
+	}
+	
+	private void methodA2() {
+		System.out.println("methodA2");
+	}
+	
+	private void methodA3() {
+		System.out.println("methodA3");
+	}
+}
